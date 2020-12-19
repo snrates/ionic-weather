3403832c3df27262e49807fa228e4caf069983c0
Soner Ateş <49957660+snrates@users.noreply.github.com>
2020-12-19 12:54:34 +0300

    add LoginPage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {User} from 'src/models/auth';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  user = {} as User;
   constructor() {}
+   Login(user:User){
+       console.log(user.username)
+   }
+RegisterGo(){      
+  console.log('click register')
+  
+}
 
 }
